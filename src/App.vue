77df20e328c32c5c815659c11296954049a942c9
Pawel <zagalapawel@gmail.com>
2019-11-13 20:21:42 +0100

    todo app

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">Lista zadań użytkownika {{ name }}</h4>
+    <div class="container-fluid p-4">
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nie masz nic do zrobienia. Wspaniale!</b>
+        </div>
+      </div>
+
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Zadanie</div>
+          <div class="col-2 font-weight-bold">Zakończono?</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{ t.action }}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input" />
+          </div>
+        </div>
+      </template>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primaty" v-on:click="addNewTodo">Dodaj!</button>
+        </div>
+      </div>
+
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">Ukryj zakończone zadania</label>
+        </div>
+
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">Usuń zkończone zadania</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      name: "Poul",
+      tasks: [
+        // { action: "Kup kwiaty", done: false },
+        // { action: "Znajdź buty", done: false },
+        // { action: "Odbierz bilety", done: true },
+        // { action: "Zadzwoń do Janka", done: false }
+      ],
+      hideCompleted: true,
+      newItemText: ""
+    };
+  },
+
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      });
+      this.storeData();
+      this.newItemText = "";
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData;
+    },
+
+    created() {
+      let data = localStorage.getItem("todos");
+      if (data != null) {
+        this.tasks = JSON.parse(data);
+      }
+    }
+  }
+};
+</script>
